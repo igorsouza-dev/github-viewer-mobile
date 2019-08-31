49c6e66912a6b583ea2af2c5c568a8fba987975a
Igor Souza <igorsouza.dev@gmail.com>
2019-08-31 07:10:53 -0300

    Initializing project

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello Worldd!</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
