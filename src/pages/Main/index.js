336e651c1117adafe9c1fe56b3ed2d3b94bcdfd2
Igor Souza <igorsouza.dev@gmail.com>
2019-08-31 14:05:12 -0300

    Added styled components. Added form elements

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Main() {
-  return <View />;
+import { Container, Form, Input, SubmitButton } from './styles';
+
+export default class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newUser: '',
+      users: [],
+    };
+  }
+
+  handleSubmit = () => {
+    console.tron.log(this.state.newUser);
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="User"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleSubmit}
+          />
+          <SubmitButton onPress={this.handleSubmit}>
+            <Icon name="add" color="#fff" size={20} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#888',
+})`
+  flex: 1;
+  height: 40px;
+  background: #eee;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: solid 1px #eee;
+`;
+
+// export const SubmitButton = styled.TouchableOpacity`
+export const SubmitButton = styled(RectButton)`
+  background-color: #7159c1;
+  border-radius: 4px;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  padding: 0 12px;
+  margin-left: 10px;
+`;
